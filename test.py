cf6d92f052f830b20e791c82e426ec6124aa7bbc
lytkarinskiy <asnaumoff@gmail.com>
2017-04-06 16:39:32 +0300

    new

--- test.py
+++ test.py
@@ -2,18 +2,9 @@ import um31
 
 um = um31.UM31()
 um.connect("/dev/ttyUSB0")
-data = um.read_current_values(1)
+data = um.read_current_values()
 
 with open("data.txt", "wb") as f:
     f.write(data)
 
-for i in um.export_json(data):
-    print(i)
-
-
-import um31
-um = um31.UM31()
-dat =open("data.txt", "rb").read()
-dat2 = um.clean_data(dat)
-dat2
-
+dat2 = um.clean_data(data)
