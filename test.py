6921d110066d189eaf09ce65e00fecd3591ea6cd
lytkarinskiy <asnaumoff@gmail.com>
2017-04-09 10:54:28 +0300

    new

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import um31
 
 um = um31.UM31()
 um.connect("/dev/ttyUSB0")
-data = um.read_current_values()
+# data = um.read_current_values()
 #
-# data = um.read_month_values(3)
+data = um.read_month_values(2)
 
 with open("data.txt", "wb") as f:
     f.write(data)
