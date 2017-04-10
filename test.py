7c71cdd2d11de442e9d0c7137659b6a236842dcf
lytkarinskiy <asnaumoff@gmail.com>
2017-04-10 14:59:10 +0300

    new

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import um31
 
 um = um31.UM31()
-um.connect("/dev/ttyUSB1")
+um.connect("/dev/ttyUSB0")
 data = um.read_diagnostic()
 #
 # data = um.read_month_values(2)
 
 with open("data.txt", "wb") as f:
     f.write(data)
-
-dat2 = um.export_json(data)
-for i in dat2:
-    print(i)
+#
+# dat2 = um.export_json(data)
+# for i in dat2:
+#     print(i)
