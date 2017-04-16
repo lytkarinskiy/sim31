00f1663834ff8ba8acc48abf00f26b4b004d28ee
lytkarinskiy <asnaumoff@gmail.com>
2017-04-16 22:40:59 +0300

    new

--- odin38g_electro.py
+++ odin38g_electro.py
@@ -1,34 +1,45 @@
-import os
 import time
 import um31
 import restreamclient
+import ssl
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
 
 location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 mqtt_broker_host = "185.41.113.138"
 mqtt_client_id = "5e9c1178-a5f0-4dc0-bbbc-d74243aab27c"
-topic = "odintcovo38g/electro"
+mqtt_topic = "odintcovo38g/electro"
+msg = []
 
 
-def job_function():
+def job_function(msg_):
     um = um31.UM31()
     um.connect("/dev/ttyUSB0")
     data = um.read_current_values()
     um.disconnect()
 
     payld = um.export_json(data)
-    msg = []
     for p in payld:
-        msg.append({"topic": topic, "payload": p})
+        msg_.append({"topic": mqtt_topic, "payload": p})
 
     mqttc = restreamclient.RestreamClient(mqtt_client_id, msg, location)
-    mqttc.client.connect(mqtt_broker_host, port=8883, keepalive=60)
-    mqttc.client.loop_forever(retry_first_connection=True)
+    try:
+        mqttc.client.connect(mqtt_broker_host, port=8883, keepalive=60)
+        mqttc.client.loop_forever(retry_first_connection=True)
+    except ssl.SSLEOFError:
+        print("ssl handshake error")
+        pass
+    except OSError as e:
+        print("OS error: {0}".format(e))
+        pass
+    else:
+        msg_ = []
+        print("msg_:", len(msg_))
+
 
 if __name__ == '__main__':
     sched = BackgroundScheduler()
-    sched.add_job(job_function, 'cron', minute='0,10,20,30,40,50')
-
+    sched.add_job(lambda: job_function(msg), 'cron', minute='0,10,20,30,40,50')
     sched.start()
 
     try:
